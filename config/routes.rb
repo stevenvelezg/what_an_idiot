65eaa441362bb2cc487378951dfc592405990a45
Steven Velez-Garcia <steven.velezgarcia@gmail.com>
2017-07-26 13:22:27 -0400

    Sign in and routes set up

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,12 @@ Rails.application.routes.draw do
 
   patch 'posts/:id/downvote' => 'posts#downvote', as: :downvote
 
+  devise_scope :user do
+    get '/sign_in' => 'devise/sessions#create'
+  end
+
+  get '/about_us' => 'statics#about_us'
+
   devise_for :users
   resources :posts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
